a46399e56fdc0704f980c11b3509c3e34e102382
rapsealk <piono623@naver.com>
2018-09-12 01:12:14 +0900

    endpoint on socket

---
+++ booster/active_socket_endpoint_tcp.cc
@@ -0,0 +1,28 @@
+#include <boost/asio.hpp>
+#include <iostream>
+using namespace boost;
+
+int main()
+{
+	unsigned short port_num = 3333;
+
+	asio::ip::tcp::endpoint ep(asio::ip::address_v4::any(), port_num);
+
+	asio::io_service ios;
+
+	asio::ip::tcp::acceptor acceptor(ios, ep.protocol());
+
+	boost::system::error_code ec;
+
+	acceptor.bind(ep, ec);
+
+	if (ec.value() != 0) {
+		std::cout << "Failed to bind the acceptor socket." << std::endl;
+		std::cout << "Error code = " << ec.value() << std::endl;
+		std::cout << "Message: " << ec.message() << std::endl;
+
+		return ec.value();
+	}
+
+	return 0;
+}
---
+++ booster/active_socket_endpoint_udp.cc
@@ -0,0 +1,28 @@
+#include <boost/asio.hpp>
+#include <iostream>
+using namespace boost;
+
+int main()
+{
+	unsigned short port_num = 3333;
+
+	asio::ip::udp::endpoint ep(asio::ip::address_v4::any(), port_num);
+
+	asio::io_service ios;
+
+	asio::ip::udp::socket sock(ios, ep.protocol());
+
+	boost::system::error_code ec;
+
+	sock.bind(ep, ec);
+
+	if (ec.value() != 0) {
+		std::cout << "Failed to bind the socket." << std::endl;
+		std::cout << "Error code = " << ec.value() << std::endl;
+		std::cout << "Message: " << ec.message() << std::endl;
+
+		return ec.value();
+	}
+
+	return 0;
+}
